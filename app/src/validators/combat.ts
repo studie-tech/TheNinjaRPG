87faa89fbe370fe18d87a6a0229acae5634db6fd
Q <trialbycombat1994@gmail.com>
2026-04-16 15:07:50 -0700

    feat(validator): add requiredBloodlineItemId to JutsuValidatorRawSchema

--- app/src/validators/combat.ts
+++ app/src/validators/combat.ts
@@ -1187,6 +1187,7 @@ export const JutsuValidatorRawSchema = z.object({
   actionCostPerc: z.coerce.number().int().min(10).max(100),
   cooldown: z.coerce.number().int().min(0).max(300),
   bloodlineId: z.string().nullable(),
+  requiredBloodlineItemId: z.string().nullable(),
   villageId: z.string().nullable(),
   effects: z.array(AllTags).superRefine(SuperRefineEffects),
   battleUsageType: z.enum(BattleUsageTypes).prefault("BOTH"),
